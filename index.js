fff87ca6774fca2065c1c6dac139623ae41a0fb3
PraneshSrinivasan <psrinivasan@fleetstudio.com>
2021-09-27 22:08:18 +0530

    initall commit

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ function requestUserRepos(owner){
             li.classList.add('list-group-item');
         
             li.innerHTML = (`
-                <p><b>Repo:</b> ${data[i].name}</p>
-                <p><b>Description:</b> ${data[i].description}</p>
-                <p><b>URL:</b> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+                <p><b> Repo: </b> ${data[i].name}</p>
+                <p><b> Description: </b> ${data[i].description}</p>
+                <p><b> URL: </b> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
             `);
             ul.appendChild(li);
         }
